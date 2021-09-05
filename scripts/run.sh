a9ef2c6d5b46d50115157639b2fe81dabdf4f9b4
nothub <48992448+nothub@users.noreply.github.com>
2021-09-05 20:43:05 +0200

    dev run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -e
+
+# cd to project root dir
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
+
+# run rootless or elevate
 docker version &>/dev/null || sudo su
+
+# cleanup
+set +e
+docker rm --force ftb
 docker rmi --force ftb:dev
+set -e
+
 docker build . --tag ftb:dev
-docker run --interactive --tty --rm --volume "$(pwd)"/data:/opt/server --env-file "$(pwd)"/.env ftb:dev
+docker run --name ftb --rm --volume "$(pwd)"/data:/opt/server --env-file "$(pwd)"/.env.example ftb:dev
